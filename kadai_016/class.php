952489920767940f03f0c025bbcf17bad43bab08
yukiminoly <52592009+yukiminoly@users.noreply.github.com>
2025-06-26 23:30:32 +0900

    kadai_016 をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,52 @@
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎16章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food {
+      private $name;
+      private $price;
+
+      public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+        print_r($this);
+        echo "<br>";
+      }
+
+      public function show_price() {
+        echo "{$this->price}<br>";
+      }
+    }
+    class Animal {
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct($name, $height, $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+        print_r($this);
+        echo "<br>";
+      }
+
+      public function show_height() {
+        echo "{$this->height}<br>";
+      }
+    }
+
+    $food = new Food("potato", 250);
+    $animal = new Animal("dog", 60, 5000);
+    echo $food->show_price();
+    echo $animal->show_height();
+    ?>
+  </p>
+</body>
+
+</html>
